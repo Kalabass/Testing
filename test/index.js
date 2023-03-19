440f3f0eed7d10ff92e11abfada7b52e689bb92c
Kalabass <sevagavgav@yandex.ru>
2023-03-20 00:32:18 +0300

    Testing

---
+++ test/index.js
@@ -0,0 +1,15 @@
+//import express from 'express';
+const express = require('express');
+const userRouter = require('./routes/user.routes');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', userRouter);
+
+
+app.listen(PORT, ()=> console.log('Server started on ' + PORT));
+
+
